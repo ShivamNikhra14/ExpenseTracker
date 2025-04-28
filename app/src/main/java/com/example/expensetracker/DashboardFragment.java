8999efe0714568b62b824e541250e52473600e5a
Shivam Nikhra <142673389+ShivamNikhra14@users.noreply.github.com>
2025-04-29 00:27:36 +0530

    added animation to floating action button

--- app/src/main/java/com/example/expensetracker/DashboardFragment.java
+++ app/src/main/java/com/example/expensetracker/DashboardFragment.java
@@ -4,61 +4,71 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.provider.DocumentsContract;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link DashboardFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+
 public class DashboardFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private FloatingActionButton fab_main_btn;
+    private FloatingActionButton fab_income_btn;
+    private FloatingActionButton fab_expense_btn;
+    private TextView fab_income_text;
+    private TextView fab_expense_text;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private boolean isOpen=false;
 
-    public DashboardFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment DashboardFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static DashboardFragment newInstance(String param1, String param2) {
-        DashboardFragment fragment = new DashboardFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private Animation FadeOpen, FadeClose;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dashboard, container, false);
+        View myview=inflater.inflate(R.layout.fragment_dashboard, container,false);
+
+        fab_main_btn=myview.findViewById(R.id.fb_main_plus_btn);
+        fab_income_btn=myview.findViewById(R.id.income_ft_btn);
+        fab_expense_btn=myview.findViewById(R.id.expense_ft_btn);
+
+        fab_income_text=myview.findViewById(R.id.income_ft_text);
+        fab_expense_text=myview.findViewById(R.id.expense_ft_text);
+
+        FadeOpen = AnimationUtils.loadAnimation(getActivity(),R.anim.fade_open);
+        FadeClose = AnimationUtils.loadAnimation(getActivity(),R.anim.fade_close);
+
+        fab_main_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (isOpen){
+                    fab_income_btn.startAnimation(FadeClose);
+                    fab_expense_btn.startAnimation(FadeClose);
+                    fab_income_btn.setClickable(false);
+                    fab_expense_btn.setClickable(false);
+                    fab_income_text.startAnimation(FadeClose);
+                    fab_expense_text.startAnimation(FadeClose);
+                    fab_income_text.setClickable(false);
+                    fab_expense_text.setClickable(false);
+                    isOpen=false;
+                }else {
+                    fab_income_btn.startAnimation(FadeOpen);
+                    fab_expense_btn.startAnimation(FadeOpen);
+                    fab_income_btn.setClickable(true);
+                    fab_expense_btn.setClickable(true);
+                    fab_income_text.startAnimation(FadeOpen);
+                    fab_expense_text.startAnimation(FadeOpen);
+                    fab_income_text.setClickable(true);
+                    fab_expense_text.setClickable(true);
+                    isOpen=true;
+                }
+            }
+        });
+
+        return myview;
     }
 }
